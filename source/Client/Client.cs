db37c51311482d5dce8957a783a195023afb4342
Andrea Milanta <andrea.milanta@gmail.com>
2018-06-05 19:52:21 +0200

    Project renamed to AmaruCommon

---
+++ source/Client/Client.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+using ClientServer.Communication;
+using ClientServer.Messages;
+
+namespace AmaruCommon.Client
+{
+    public class Client : ClientTCP
+    {
+        public Client(Socket soc, int bufferSize, string log) : base(soc, bufferSize, log)
+        {
+        }
+
+        protected override void HandleNewMessage(Message mex)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- source/Constants/NetworkConstants.cs
+++ source/Constants/NetworkConstants.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AmaruComm.Constants
+namespace AmaruCommon.Constants
 {
-    class NetworkConstants
+    public static class NetworkConstants
     {
+        public const int BufferSize = 1024;         // Network Receiving Buffer size
+        public const int ServerPort = 5555;         // Main Server Receiving Port
+
     }
 }
