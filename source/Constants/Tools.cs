65268ebc7b03b53f317f59b8439906e8b58c7241
Andrea Milanta <andrea.milanta@gmail.com>
2018-06-19 11:40:01 +0200

    Shuffling function fixed

--- source/Constants/Tools.cs
+++ source/Constants/Tools.cs
@@ -8,15 +8,21 @@ namespace AmaruCommon.Constants
 {
     public static class Tools
     {
+        private static Random rng = new Random();
         /// <summary>
         /// Shuffle lists
         /// </summary>
         /// <param name="list"></param>
         public static List<T> Shuffle<T>(List<T> list)
         {
-            Random rand = new Random();
-            int len = list.Count * list.Count;
-            list.OrderBy(e => rand.Next(len));
+            int n = list.Count;  
+            while (n > 1) {  
+                n--;  
+                int k = rng.Next(n + 1);  
+                T value = list[k];  
+                list[k] = list[n];  
+                list[n] = value;  
+            }
             return list;
         }
     }
