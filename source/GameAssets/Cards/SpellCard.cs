3148e6cabb047f4189dca7576c2ff18c1348c39a
Simone Amico <simoamico94@hotmail.it>
2018-06-20 16:54:55 +0200

    123

--- source/GameAssets/Cards/SpellCard.cs
+++ source/GameAssets/Cards/SpellCard.cs
@@ -12,7 +12,7 @@ namespace AmaruCommon.GameAssets.Cards
     {
         public SpellAbility Effect { get; private set; }
 
-        public SpellCard(string name, int cost, SpellAbility effect = null, int id = AmaruConstants.AUTO_ID) : base(cost, name, id)
+        public SpellCard(string name, int cost, SpellAbility effect = null, int id = AmaruConstants.AUTO_ID) : base(id, name, cost)
         {
             this.Effect = effect;
         }
