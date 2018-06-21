bd20eecb3c6251c5dcde95a84ff0a36cf8b7637c
Marco Faleri <marco.faleri@studenti.unimi.it>
2018-06-21 19:59:46 +0200

    Some Exception

---
+++ source/Exceptions/InvalidCardTypeException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace AmaruServer.Game.Managing
+{
+    [Serializable]
+    public class InvalidCardTypeException : Exception
+    {
+        public InvalidCardTypeException()
+        {
+        }
+    }
+}
